07acde45b1e2dd1932f3567fd6c4000d72280e4b
Inhji <johnnie@posteo.de>
2018-11-19 18:00:26 +0100

    fix status message for webmentions in error case

--- lib/akedia_web/helpers/webmentions.ex
+++ lib/akedia_web/helpers/webmentions.ex
@@ -1,15 +1,12 @@
 defmodule AkediaWeb.Helpers.Webmentions do
   def send_webmentions(url, type, action) do
-    urls =
-      case Webmentions.send_webmentions(url) do
-        {:ok, list} ->
-          list
+    case Webmentions.send_webmentions(url) do
+      {:ok, list} ->
+        "#{type} #{action} successfully. Webmentions sent to these endpoints:\n" <>
+          Webmentions.results_as_text(list)
 
-        {:error, reason} ->
-          [reason]
-      end
-
-    "#{type} #{action} successfully. Webmentions sent to these endpoints:\n" <>
-      Webmentions.results_as_text(urls)
+      {:error, reason} ->
+        "#{type} #{action} failed! Reason: \n" <> reason
+    end
   end
 end
