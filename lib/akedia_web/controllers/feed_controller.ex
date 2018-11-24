7ad65771dc66fd47a38144ff5070f4b33fe07aa2
Inhji <johnnie@posteo.de>
2018-11-25 00:02:09 +0100

    update atom feed

--- lib/akedia_web/controllers/feed_controller.ex
+++ lib/akedia_web/controllers/feed_controller.ex
@@ -5,30 +5,33 @@ defmodule AkediaWeb.FeedController do
   alias Akedia.Posts.Post
 
   defp author, do: "Inhji"
-  defp base_url, do: "https://inhji.de/"
 
-  def build_feed(posts) do
-    Feed.new(base_url(), DateTime.utc_now(), "Inhji.de Atom Feed")
+  def build_feed(conn, posts) do
+    base_url = Routes.page_url(conn, :index)
+
+    Feed.new(base_url, DateTime.utc_now(), "Inhji.de Atom Feed")
+    |> Feed.logo(Routes.static_path(conn, "/images/logo.png"))
     |> Feed.author(author(), email: "inhji@posteo.de")
-    |> Feed.link("#{base_url()}feed", rel: "self")
-    |> Feed.entries(Enum.map(posts, &get_entry/1))
+    |> Feed.link(Routes.feed_url(conn, :index), rel: "self")
+    |> Feed.entries(
+      Enum.map(posts, fn %Post{} = post ->
+        Entry.new(
+          Routes.post_url(conn, :show, post.id),
+          DateTime.from_naive!(post.inserted_at, "Etc/UTC"),
+          post.excerpt || "Title"
+        )
+        |> Entry.link(Routes.post_url(conn, :show, post.id))
+        |> Entry.author(author(), uri: base_url)
+        |> Entry.content(post.content_html, type: "html")
+        |> Entry.build()
+      end)
+    )
     |> Feed.build()
     |> Atomex.generate_document()
   end
 
-  defp get_entry(%Post{} = post) do
-    Entry.new(
-      "#{base_url()}posts/#{post.id}",
-      DateTime.from_naive!(post.inserted_at, "Etc/UTC"),
-      post.excerpt || "Title"
-    )
-    |> Entry.author(author(), uri: base_url())
-    |> Entry.content(post.content_html, type: "html")
-    |> Entry.build()
-  end
-
   def index(conn, _params) do
-    feed = build_feed(Posts.list_posts())
+    feed = build_feed(conn, Posts.list_posts())
     html(conn, feed)
   end
 end
