4b6efcab34a4b1dc547dc2ae323cf1330b5566d1
Inhji <johnnie@posteo.de>
2018-11-24 13:45:20 +0100

    log sending of webmentions

--- lib/akedia_web/helpers/webmentions.ex
+++ lib/akedia_web/helpers/webmentions.ex
@@ -1,12 +1,18 @@
 defmodule AkediaWeb.Helpers.Webmentions do
+  require Logger
+
   def send_webmentions(url, type, action) do
+    Logger.info("Sending webmentions for: #{url}")
+
     case Webmentions.send_webmentions(url) do
       {:ok, list} ->
+        Logger.info("Webmention sent: #{Webmentions.results_as_text(list)}")
+
         "#{type} #{action} successfully. Webmentions sent to these endpoints:\n" <>
           Webmentions.results_as_text(list)
 
       {:error, reason} ->
-        IO.inspect(reason)
+        Logger.info("Webmention failed: #{reason}")
         "#{type} #{action} failed!"
     end
   end
