8a50be76d9ab1e744954a7cdee6b345cd8662778
Inhji <johnnie@posteo.de>
2018-11-25 14:10:48 +0100

    remove public layout

--- lib/akedia_web/controllers/page_controller.ex
+++ lib/akedia_web/controllers/page_controller.ex
@@ -1,8 +1,6 @@
 defmodule AkediaWeb.PageController do
   use AkediaWeb, :controller
 
-  plug :put_layout, :public when action in [:index]
-
   def index(conn, _params) do
     posts = Enum.take(Akedia.Posts.list_posts(), 3)
 
