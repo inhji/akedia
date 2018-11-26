2fc828a81407b27a9651513513f9cd7c8c7c260b
Inhji <johnnie@posteo.de>
2018-11-26 10:39:26 +0100

    set content type of atom feed

--- lib/akedia_web/controllers/feed_controller.ex
+++ lib/akedia_web/controllers/feed_controller.ex
@@ -4,14 +4,20 @@ defmodule AkediaWeb.FeedController do
   alias Akedia.Posts
   alias Akedia.Posts.Post
 
-  defp author, do: "Inhji"
+  def index(conn, _params) do
+    feed = build_feed(conn, Posts.list_posts())
 
-  def build_feed(conn, posts) do
+    conn
+    |> put_resp_content_type("application/atom+xml")
+    |> html(feed)
+  end
+
+  defp build_feed(conn, posts) do
     base_url = Routes.page_url(conn, :index)
 
     Feed.new(base_url, DateTime.utc_now(), "Inhji.de Atom Feed")
     |> Feed.logo(Routes.static_path(conn, "/images/logo.png"))
-    |> Feed.author(author(), email: "inhji@posteo.de")
+    |> Feed.author("Jonathan Jenne", email: "inhji@posteo.de")
     |> Feed.link(Routes.feed_url(conn, :index), rel: "self")
     |> Feed.entries(
       Enum.map(posts, fn %Post{} = post ->
@@ -21,17 +27,12 @@ defmodule AkediaWeb.FeedController do
           post.excerpt || "Title"
         )
         |> Entry.link(Routes.post_url(conn, :show, post.id))
-        |> Entry.author(author(), uri: base_url)
+        |> Entry.author("Jonathan Jenne", uri: base_url)
         |> Entry.content(post.content_html, type: "html")
         |> Entry.build()
       end)
     )
     |> Feed.build()
     |> Atomex.generate_document()
-  end
-
-  def index(conn, _params) do
-    feed = build_feed(conn, Posts.list_posts())
-    html(conn, feed)
   end
 end
