b0874fdcad348ba0e5eaee263dfdee1ed4042e25
Inhji <johnnie@posteo.de>
2018-12-05 20:58:41 +0100

    mark github POSSE posts \o/

--- lib/akedia_web/views/post_view.ex
+++ lib/akedia_web/views/post_view.ex
@@ -3,31 +3,76 @@ defmodule AkediaWeb.PostView do
   alias Akedia.Posts.PostImage
   import Scrivener.HTML
 
+  @github_new_issue ~r/^https:\/\/github.com\/(?<user>\w+)\/(?<repo>\w+)\/issues/
+  @github_issue_comment ~r/^https:\/\/github.com\/(?<user>\w+)\/(?<repo>\w+)\/issues\/(?<issue>\d+)/
+
   def image(post, version \\ :thumb) do
     PostImage.url({post.image, post}, version)
   end
 
-  def has_image?(post) do
-    !!post.image
+  def is_type?(post, type \\ "note"), do: post.type === type
+
+  def is_note?(post), do: is_type?(post)
+
+  def is_reply?(post), do: is_type?(post, "reply")
+
+  def has_image?(post), do: !!post.image
+
+  def has_title?(post), do: !!post.title
+
+  def content_class(post), do: if(has_title?(post), do: "", else: "p-name")
+
+  def is_github_action?(post),
+    do: is_github_issue_comment?(post) || is_new_github_issue?(post)
+
+  def github_action(post) do
+    cond do
+      is_github_issue_comment?(post) ->
+        link = safe_to_string(link("issue", to: post.in_reply_to))
+        raw("commented on an #{link} on #{github_repo_name(post)}")
+
+      is_new_github_issue?(post) ->
+        link = safe_to_string(link(github_repo_name(post), to: post.in_reply_to))
+        raw("created an issue on #{link}")
+    end
   end
 
-  def has_title?(post) do
-    !!post.title
+  def is_github_issue_comment?(post) do
+    is_reply?(post) && Regex.match?(@github_issue_comment, post.in_reply_to)
   end
 
-  def content_class(post) do
-    if has_title?(post), do: "", else: "p-name"
+  def is_new_github_issue?(post) do
+    is_reply?(post) && Regex.match?(@github_new_issue, post.in_reply_to)
+  end
+
+  def github_repo_name(post) do
+    %{"user" => user, "repo" => repo} = Regex.named_captures(@github_new_issue, post.in_reply_to)
+    "#{user}/#{repo}"
   end
 
   def ribbon(post) do
     {class, icon} =
       case post.type do
-        "reply" -> {"is-success", "fas fa-comment"}
-        "like" -> {"is-danger", "fas fa-heart"}
-        "bookmark" -> {"is-primary", "fas fa-bookmark"}
-        "repost" -> {"is-warning", "fas fa-recycle"}
-        "article" -> {"is-link", "fas fa-pen-nib"}
-        _ -> {nil, nil}
+        "reply" ->
+          cond do
+            is_github_action?(post) -> {"is-success", "fab fa-github"}
+            true -> {"is-success", "fas fa-comment"}
+          end
+
+        "like" ->
+          {"is-danger", "fas fa-heart"}
+
+        "bookmark" ->
+          {"is-primary", "fas fa-bookmark"}
+
+        "repost" ->
+          {"is-warning", "fas fa-recycle"}
+
+        "article" ->
+          {"is-link", "fas fa-pen-nib"}
+
+        _ ->
+          {"is-dark", "fas fa-sticky-note"}
       end
 
     cond do
