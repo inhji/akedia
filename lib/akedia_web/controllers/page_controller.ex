3644963f37c726b9886781215ca3640ef7257624
Inhji <johnnie@posteo.de>
2018-12-16 14:07:49 +0100

    show tracks and artists on now page

--- lib/akedia_web/controllers/page_controller.ex
+++ lib/akedia_web/controllers/page_controller.ex
@@ -1,7 +1,16 @@
 defmodule AkediaWeb.PageController do
   use AkediaWeb, :controller
+  alias Akedia.Tracks
 
   def now(conn, _params) do
-    render(conn, "now.html")
+    tracks = Tracks.list_tracks_for(:today)
+
+    artists =
+      Tracks.list_artists_for(:today)
+      |> Enum.sort_by(fn {_, count} -> count end, &>/2)
+
+    IO.inspect(artists)
+
+    render(conn, "now.html", tracks: tracks, artists: artists)
   end
 end
