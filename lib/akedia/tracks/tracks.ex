a2b1a613f1ee422b870d0d53fa537c21dc293d25
Inhji <johnnie@posteo.de>
2018-12-16 14:07:28 +0100

    track query methods

--- lib/akedia/tracks/tracks.ex
+++ lib/akedia/tracks/tracks.ex
@@ -8,6 +8,40 @@ defmodule Akedia.Tracks do
     Repo.all(Track)
   end
 
+  def list_tracks_for(:today, limit \\ 10) do
+    query = tracks_for(get_today())
+
+    Repo.all(
+      from(t in query,
+        order_by: [desc: :listened_at],
+        limit: ^limit
+      )
+    )
+  end
+
+  def list_artists_for(:today) do
+    query = tracks_for(get_today())
+
+    Repo.all(
+      from(t in query,
+        group_by: t.artist,
+        select: {t.artist, count(t.id)}
+      )
+    )
+  end
+
+  def tracks_for(day) do
+    from(t in Track,
+      where: t.listened_at >= ^Timex.beginning_of_day(day),
+      where: t.listened_at < ^Timex.end_of_day(day)
+    )
+  end
+
+  defp get_today() do
+    Date.utc_today()
+    |> Timex.to_datetime()
+  end
+
   def create_track(attrs \\ %{}) do
     %Track{}
     |> Track.changeset(attrs)
