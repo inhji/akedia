b34b51ad4c9aa45026c8ecb04f7582a6ed99533d
Inhji <johnnie@posteo.de>
2018-12-22 16:52:20 +0100

    add seed for listens

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+contents = File.read!("./data.json")
+list = Jason.decode!(contents)
+
+list
+|> Enum.map(fn item ->
+  %{
+    :artist => item["artist_name"],
+    :timestamp => item["timestamp"],
+    :album => item["release_name"],
+    :name => item["track_name"],
+    :listened_at =>
+      (item["timestamp"] * 1000)
+      |> DateTime.from_unix!(:millisecond)
+  }
+end)
+|> Enum.each(&Akedia.Tracks.create_track/1)
