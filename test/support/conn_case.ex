583914a84b9913263b64714496f82d29c27bf756
Jonathan Jenne <johnnie@posteo.de>
2018-12-29 10:32:49 +0100

    clean up conn_case

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -29,39 +29,6 @@ defmodule AkediaWeb.ConnCase do
     end
   end
 
-  # setup_all do
-  #   :ok = Ecto.Adapters.SQL.Sandbox.checkout(Akedia.Repo)
-  #   # we are setting :auto here so that the data persists for all tests,
-  #   # normally (with :shared mode) every process runs in a transaction
-  #   # and rolls back when it exits. setup_all runs in a distinct process
-  #   # from each test so the data doesn't exist for each test.
-  #   Ecto.Adapters.SQL.Sandbox.mode(Akedia.Repo, :auto)
-  #
-  #   {:ok, user} =
-  #     Akedia.Accounts.create_user(%{
-  #       encrypted_password: "some encrypted_password",
-  #       username: "some username"
-  #     })
-  #
-  #   on_exit(fn ->
-  #     # this callback needs to checkout its own connection since it
-  #     # runs in its own process
-  #     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Akedia.Repo)
-  #     Ecto.Adapters.SQL.Sandbox.mode(Akedia.Repo, :auto)
-  #
-  #     # we also need to re-fetch the %Tenant struct since Ecto otherwise
-  #     # complains it's "stale"
-  #
-  #     user = Akedia.Accounts.get_user!(user.id)
-  #     Akedia.Accounts.delete_user(user)
-  #     # Akedia.Repo.delete_all(Akedia.Accounts.User)
-  #
-  #     :ok
-  #   end)
-  #
-  #   [user: user]
-  # end
-
   setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Akedia.Repo)
 
