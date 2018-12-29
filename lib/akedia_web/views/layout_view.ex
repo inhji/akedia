e9e3b93e9c792a063b982ea9939e835666bc2b0c
Jonathan Jenne <johnnie@posteo.de>
2018-12-30 00:37:36 +0100

    first naíve page_title implementation

--- lib/akedia_web/views/layout_view.ex
+++ lib/akedia_web/views/layout_view.ex
@@ -1,5 +1,52 @@
 defmodule AkediaWeb.LayoutView do
   use AkediaWeb, :view
+  alias AkediaWeb.PageController
+  alias AkediaWeb.PostController
+  alias AkediaWeb.ImageController
+
+  def page_title(conn) do
+    IO.inspect(conn)
+
+    %{
+      :phoenix_action => action,
+      :phoenix_controller => controller
+    } = conn.private
+
+    title = case controller do
+      PageController ->
+        case action do
+          :index -> "Home"
+          :now -> "Now"
+          _ -> nil
+        end
+      PostController ->
+        case action do
+          :index -> "Posts"
+          :by_type ->
+            post_type = conn.params["type"]
+            String.capitalize(post_type) <> "s"
+          :show ->
+            post_id = conn.params["id"]
+            "Post #{post_id}"
+          _ -> nil
+        end
+      ImageController ->
+        case action do
+          :index -> "Photos"
+          :show ->
+            image_id = conn.params["id"]
+            "Photo #{image_id}"
+          _ -> nil
+        end
+      _ -> nil
+    end
+
+    if is_nil(title) do
+      "Technos & Psyche"
+    else
+      "#{title} - Technos & Psyche"
+    end
+  end
 
   def page_subtitle do
     subtitles = [
